d4ae7e068907e41c7cfb26d868fc60df8077f398
Brandon Newton <btnwtn@gmail.com>
2017-10-19 14:58:41 -0400

    Update to es6

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,40 +1,42 @@
-Stripe.setPublishableKey('UPDATE ME');
+Stripe.setPublishableKey("UPDATE ME");
 
-$(document).on('ready', function() {
-  $('form-errors').hide();
+$(document).on("ready", () => {
+  $("form-errors").hide();
 });
 
-$('#product-form').on('submit', function(event){
-
+$("#product-form").on("submit", event => {
   event.preventDefault();
 
-  $('form-errors').hide();
+  $("form-errors").hide();
 
-  Stripe.card.createToken({
-    number: $('#card-number').val(),
-    cvc: $('#cvv').val(),
-    exp_month: $('#expiry-month').val(),
-    exp_year: $('#expiry-year').val()
-  }, stripeResponseHandler);
+  Stripe.card.createToken(
+    {
+      number: $("#card-number").val(),
+      cvc: $("#cvv").val(),
+      exp_month: $("#expiry-month").val(),
+      exp_year: $("#expiry-year").val()
+    },
+    stripeResponseHandler
+  );
 
-  $('#submit-btn').prop("disabled", true);
-
+  $("#submit-btn").prop("disabled", true);
 });
 
-function stripeResponseHandler(status, response) {
-  var $form = $('#product-form');
+const stripeResponseHandler = (status, response) => {
+  const $form = $("#product-form");
+
   if (response.error) {
     // Show the errors on the form
-    $('#form-errors').show();
-    $('#form-errors').html(response.error.message);
-    $('#submit-btn').prop("disabled", false);
+    $("#form-errors").show();
+    $("#form-errors").html(response.error.message);
+    $("#submit-btn").prop("disabled", false);
   } else {
     // response contains id and card, which contains additional card details
-    var token = response.id;
+    const token = response.id;
     // Insert the token into the form so it gets submitted to the server
     $form.append($('<input type="hidden" name="stripeToken" />').val(token));
     // and submit
     console.log($form.get(0));
     $form.get(0).submit();
   }
-}
+};
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,49 +1,51 @@
-var express = require('express');
-var router = express.Router();
-var stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const express = require("express");
+const router = express.Router();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-var products = [
+const products = [
   {
-    productName: 'Soylent',
-    productDescription: 'Meal replacement that tastes like pancake batter.',
+    productName: "Soylent",
+    productDescription: "Meal replacement that tastes like pancake batter.",
     productPrice: 10.99
   }
 ];
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Express" });
 });
 
-router.get('/products/:name', function(req, res, next) {
-  var productName = req.params.name;
-  for (var i = 0; i < products.length; i++) {
-    if(productName === products[i].productName) {
-      return res.render('product', {productInfo: products[i]});
+router.get("/products/:name", (req, res, next) => {
+  const productName = req.params.name;
+  for (let i = 0; i < products.length; i++) {
+    if (productName === products[i].productName) {
+      return res.render("product", { productInfo: products[i] });
     } else {
-      return res.send('Product does not exist.');
+      return res.send("Product does not exist.");
     }
   }
 });
 
-router.post('/charge', function(req, res,next) {
-  var stripeToken = req.body.stripeToken;
-  var amount = req.body.price * 100;
+router.post("/charge", (req, res, next) => {
+  const stripeToken = req.body.stripeToken;
+  const amount = req.body.price * 100;
 
   // ensure amount === actual product amount to avoid fraud
 
-  stripe.charges.create({
-    card: stripeToken,
-    currency: 'usd',
-    amount: amount
-  },
-  function(err, charge) {
-    if (err) {
-      console.log(err);
-      res.send('error');
-    } else {
-      res.send('success');
+  stripe.charges.create(
+    {
+      card: stripeToken,
+      currency: "usd",
+      amount
+    },
+    (err, charge) => {
+      if (err) {
+        console.log(err);
+        res.send("error");
+      } else {
+        res.send("success");
+      }
     }
-  });
+  );
 });
 
-module.exports = router;
+export default router;
